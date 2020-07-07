28ef4c24d2cc054f662c2a9207d90d326b996b75
MarisabelC <marisabelchang@gmail.com>
2020-07-06 20:20:57 -0700

    init commit

---
+++ app/src/main/java/com/example/screenshotapp/Coordinates.java
@@ -0,0 +1,12 @@
+package com.example.screenshotapp;
+
+public class Coordinates {
+    int x;
+    int y;
+
+    public Coordinates(int x,int y){
+        this.x = x;
+        this.y = y;
+
+    }
+}
